15c5ea8be8fd82502a120bf5151585510b552e3d
danush4756-ai <danush4756@gmail.com>
2026-03-22 07:29:13 +0530

    CommonToAllTest

---
+++ src/test/java/com/thetestingacademy/BaseTest/CommonToAllTest.java
@@ -0,0 +1,14 @@
+package com.thetestingacademy.BaseTest;
+import com.thetestingacademy.driver.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+public class CommonToAllTest {
+    @BeforeMethod
+    public void setUp(){
+        DriverManager.init();
+    }
+    @AfterMethod
+    public void tearDown(){
+        DriverManager.down();
+    }
+}
